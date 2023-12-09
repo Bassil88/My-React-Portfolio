73049be1d9f01a6c43012aa5bac024b7e726e034
Bassilio <bassilede88@gmail.com>
2023-12-09 22:45:37 +0100

    jQuery project added

--- src/components/projectsData/projectData.js
+++ src/components/projectsData/projectData.js
@@ -5,18 +5,27 @@ const projectData = [
       "https://images.unsplash.com/photo-1533709752211-118fcaf03312?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     link: "https://bassil88.github.io/my-first-web-page/",
   },
+
+
+
   {
-    title: "First Challenge on Frontendmentor",
+    title: "Simon (jQuery)",
     image:
       "https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGNvZGRpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-    link: "https://bassil88.github.io/3-column-preview-card-component/",
+    link: "https://bassil88.github.io/Simon-Game-jQuery/",
   },
 
   {
-    title: "Fitness-Tracker",
+    title: "Fitness-Tracker (API)",
     image:
       "https://images.unsplash.com/photo-1649443992089-8bf1fc3c42f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     link: "https://workout-tracker-react-app.netlify.app/",
+  },
+  {
+    title: "Music Kit",
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg",
+    link: "https://bassil88.github.io/music-kit/",
   },
   {
     title: "Tindog (BootStrap)",
@@ -36,24 +45,19 @@ const projectData = [
       "https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     link: "https://bassil88.github.io/Dice-Game-v1/",
   },
-  {
-    title: "Music Kit",
-    image:
-      "https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg",
-    link: "https://bassil88.github.io/music-kit/",
-  },
-  {
-    title: "Donates cake web page",
-    image:
-      "https://images.unsplash.com/photo-1553524913-efba3f0b533e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    link: "https://bassil88.github.io/Sweety-layout-challenged-solved/",
-  },
-  {
-    title: "Grid",
-    image:
-      "https://images.unsplash.com/photo-1517134191118-9d595e4c8c2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8fGNvZGRpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-    link: "https://bassil88.github.io/Grid-layout-document-Challenged-solved/",
-  },
+
+  // {
+  //   title: "Donates cake web page",
+  //   image:
+  //     "https://images.unsplash.com/photo-1553524913-efba3f0b533e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  //   link: "https://bassil88.github.io/Sweety-layout-challenged-solved/",
+  // },
+  // {
+  //   title: "Grid",
+  //   image:
+  //     "https://images.unsplash.com/photo-1517134191118-9d595e4c8c2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8fGNvZGRpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  //   link: "https://bassil88.github.io/Grid-layout-document-Challenged-solved/",
+  // },
   {
     title: "Neumorphic music-player Design",
     image:
@@ -63,21 +67,28 @@ const projectData = [
   {
     title: "chessboard layout challenge",
     image:
-      "https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      "https://images.unsplash.com/photo-1553524913-efba3f0b533e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     link: "https://bassil88.github.io/chessboard-layout-challenge/",
   },
   {
-    title: "Movie Web Page",
+    title: "First Challenge on Frontendmentor",
     image:
-      "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    link: "https://bassil88.github.io/Moive-Web-Page/",
+    "https://images.unsplash.com/photo-1517134191118-9d595e4c8c2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8fGNvZGRpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+
+    link: "https://bassil88.github.io/3-column-preview-card-component/",
   },
-  {
-    title: "Basic-Coffee-webpage",
-    image:
-      "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    link: "https://bassil88.github.io/Basic-Coffee-webpage/",
-  },
+  // {
+  //   title: "Movie Web Page",
+  //   image:
+  //     "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  //   link: "https://bassil88.github.io/Moive-Web-Page/",
+  // },
+  // {
+  //   title: "Basic-Coffee-webpage",
+  //   image:
+  //     "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  //   link: "https://bassil88.github.io/Basic-Coffee-webpage/",
+  // },
 ];
 
 export default projectData;
